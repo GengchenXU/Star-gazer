96f853187e1d315102eb6a65e73f643f5d9cf7d4
Mr.Xu <syxx46xgc@126.com>
2020-08-14 23:02:12 +0800

    szdgasdg

--- C-practice/流感.c
+++ C-practice/流感.c
@@ -1,27 +1,25 @@
 #include<stdio.h>
 int main(){
 	int i,j,n,m,e,num,o,h;
-     scanf("%d",&n);
+        scanf("%d",&n);
     char ss[100][100];
- int hzb[100]={0},zzb[100]={0};
- for(i=0;i<n;i++)
-    {
-       scanf("%s",ss[i]);
+    int hzb[100]={0},zzb[100]={0};
+    for(i=0;i<n;i++){
+        scanf("%s",ss[i]);
     }
- scanf("%d",&m);
-   while(m>1)
+    scanf("%d",&m);
+    while(m>1)
     {
         e=0;
- for(i=0;i<n;i++)
+        for(i=0;i<n;i++)
         {
             for(j=0;j<n;j++)
             {
-                    if(ss[i][j]=='@')
-                {
+                    if(ss[i][j]=='@'){
                     hzb[e]=i;
                     zzb[e]=j;
                     e++;//寻找第一天得流感的人
-		                    }
+		            }
             }
         }
 
@@ -41,7 +39,7 @@ int main(){
             if(h+1<=n-1&&ss[o][h+1]=='.')
             {
                 ss[o][h+1]='@';
-           }//判断病人，邻居，下一行，是否有人，有人，另感染
+            }//判断病人，邻居，下一行，是否有人，有人，另感染
             if(o+1<=n-1&&ss[o+1][h]=='.')
             {
                 ss[o+1][h]='@';
@@ -56,10 +54,10 @@ int main(){
         {
             if(ss[i][j]=='@')
             {
-               num++;
+                num++;
             }
         }
     }
     printf("%d\n",num);
-   return 0;
+    return 0;
 }
