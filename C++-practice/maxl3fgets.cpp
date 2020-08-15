359769d247bc03af195f70c053b6ad66fa11bea3
Mr.Xu <syxx46xgc@126.com>
2020-08-15 22:42:34 +0800

    tae

--- C++-practice/maxl3fgets.cpp
+++ C++-practice/maxl3fgets.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int maxlen=0;
     char s[80];
-   fgets(s,80,stdin); 
+    fgets(s,80,stdin); 
     int tmplen=0;
     int last=0;
     for(int i=0;s[i]!='\0';i++)
@@ -15,24 +15,20 @@ int main()
             {
                 maxlen=tmplen;
                 last=i;
-                            
-	         break;
-	       }
-	}
-       	else if(s[i]==' ')
-	    {
-	    if(maxlen<=tmplen)
-            {
-                maxlen=tmplen;
-                tmplen=0;
-		last=i;
-                            }
-	 
-		     }
-	else tmplen++;	     
-   		}
-        for(int i=last-maxlen;i<last;i++)
+                break;
+	        }
+	    }
+        else if(s[i]==' '){
+	    if(maxlen<=tmplen){
+            maxlen=tmplen;
+            tmplen=0;
+		    last=i;
+        }
+        }
+	    else tmplen++;
+    }
+    for(int i=last-maxlen;i<last;i++)
         cout << s[i] ;
-    	return 0;
+    return 0;
 }
 
