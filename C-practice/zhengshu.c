65d8b08402f6b51922e87eeca659f0386b6157eb
Mr.XU <syxx46xgc@126.com>
2020-09-06 17:14:34 +0800

    9/6

--- C-practice/zhengshu.c
+++ C-practice/zhengshu.c
@@ -7,13 +7,13 @@ int main()
         scanf("%d", &a[i]);
     for (int i = 0; i < n - 1; i ++)
         for (int j = 1; j < n - i; j ++)
-        	if (a[j - 1] > a[j]) {
-        		int temp = a[j];
-        		a[j] = a[j - 1];
-        		a[j - 1] = temp;
+            if (a[j - 1] > a[j]) {
+                int temp = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = temp;
     		}
     for (int i = 0; i < n; i ++)
-       printf("%d\n",a[i]);
+        printf("%d\n",a[i]);
     return 0;
 }
 
