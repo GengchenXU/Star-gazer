0199bd15ffb5516d5938515c55eb6233887068a9
Mr.XU <syxx46xgc@126.com>
2020-09-06 17:09:00 +0800

    fuck

--- C-practice/zhengchu4.c
+++ C-practice/zhengchu4.c
@@ -6,21 +6,16 @@ int main ()
 		scanf("%d",&a[i]); 
 		for (int i=0;i<5;i++) {
 			
-    if (a[i]%3!=0&&a[i]%5!=0&&a[i]%7!=0)   
-     {printf("n\n");
-     
-     return 0;
-     }  
+    if (a[i]%3!=0&&a[i]%5!=0&&a[i]%7!=0)   {
+        printf("n\n");
+        return 0;
+    }  
     else{
-	    if (a[i]%3==0) 
-	    printf("3 ");  
-            if (a[i]%5==0) 
-	    printf("5 ");  
-            if (a[i]%7==0) 
-	    printf("7"); 
-	    printf("\n");}
-	    
-	    } 
+        if (a[i]%3==0) printf("3 ");  
+        if (a[i]%5==0) printf("5 ");  
+        if (a[i]%7==0) printf("7"); 
+        printf("\n");}
+    } 
     return 0;  
 }  
 
